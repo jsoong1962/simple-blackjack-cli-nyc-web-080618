9b489c4d40166e689004f0235b5331954a273831
Johnny Soong <johnnysoong@hotmail.com>
2018-07-31 16:30:22 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -48,14 +48,14 @@ end
 
 def hit?(total)
   # # code hit? here
-  # prompt_user
-  # user_input = get_user_input
-  # if user_input == 'h'
-  #   total += deal_card
-  # elsif user_input == 's'
-  #   total 
-  # else
-  #   invalid_command
-  #   prompt_user
-  # end
+  prompt_user
+  user_input = get_user_input
+  if user_input == 'h'
+    total += deal_card
+  elsif user_input == 's'
+  total 
+   else
+   invalid_command
+   prompt_user
+  end
 end
