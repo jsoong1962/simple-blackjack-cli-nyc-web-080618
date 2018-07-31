48a5329236c161531224bcce1f9ccab45d86ab34
Johnny Soong <johnnysoong@hotmail.com>
2018-07-31 16:28:13 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -45,15 +45,15 @@ def invalid_command
   puts "Please enter a valid command"
 
 def hit?(total)
-  # code hit? here
-  prompt_user
-  user_input = get_user_input
-  if user_input == 'h'
-    total += deal_card
-  elsif user_input == 's'
-    total 
-  else
-    invalid_command
-    prompt_user
-  end
+  # # code hit? here
+  # prompt_user
+  # user_input = get_user_input
+  # if user_input == 'h'
+  #   total += deal_card
+  # elsif user_input == 's'
+  #   total 
+  # else
+  #   invalid_command
+  #   prompt_user
+  # end
 end
